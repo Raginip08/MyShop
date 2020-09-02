feecc44e2a516c0043c2198476562fd73bf44a82
Ragini Patidar <ragini.patidar@accenture.com>
2020-09-03 00:53:05 +0530

    Committing models calss for products

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+using MyShop.Core;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+        ProductRepository()
+        {
+            products = (List<Product>)cache["produsts"];
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        public void commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate.Name = product.Name;
+                productToUpdate.Description = product.Description;
+                productToUpdate.Price = product.Price;
+                productToUpdate.Category = product.Category;
+                productToUpdate.Image = product.Image;
+            }
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = products.Find(p => p.Id == product.Id);
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+        }
+
+        public Product Find(Product product)
+        {
+            Product productToFind = products.Find(p => p.Id == product.Id);
+            if (productToFind != null)
+            {
+                return productToFind;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+    }
+}
