a95f7c6bf17d78a2e5b4e026130ed4eebb9c0639
Ragini Patidar <ragini.patidar@accenture.com>
2020-09-06 02:43:22 +0530

    Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext: DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductCategory> categories { get; set; }
+    }
+}
